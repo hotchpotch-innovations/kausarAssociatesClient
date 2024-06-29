db8a5447d7e5c1b287f09c199135e9b6aad37ed0
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-06-29 13:31:21 +0600

    [ADD] About us section working process loading cards added

---
+++ src/components/common/loader/ConsultancyCardLoader.jsx
@@ -0,0 +1,7 @@
+const ConsultancyCardLoader = () => {
+  return (
+    <div className="max-w-full h-[250px] 2xl:h-[300px] sm:max-w-[450px] 2xl:max-w-[500px] rounded-xl bg-gray-100" />
+  );
+};
+
+export default ConsultancyCardLoader;
